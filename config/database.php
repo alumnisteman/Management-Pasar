22c849bb8b2d8fae782eeeb4187e8adfc46d895b
Admin Alumni <admin@alumnisteman.com>
2026-09-20 08:25:52 +0700

    Fix Str::slug namespace in config/database.php

--- config/database.php
+++ config/database.php
@@ -34,7 +34,7 @@ return [
         'client' => env('REDIS_CLIENT', 'phpredis'),
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', \Illuminate\Support\Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
         'default' => [
             'url' => env('REDIS_URL'),
